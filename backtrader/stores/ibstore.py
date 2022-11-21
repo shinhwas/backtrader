b2af3b3cccc21eca5764f712dc048e6cf689b86e
shinhwas <shinhwas.us@gmail.com>
2022-11-20 22:56:24 -0800

    bug fix

--- backtrader/stores/ibstore.py
+++ backtrader/stores/ibstore.py
@@ -1378,12 +1378,11 @@ class IBStore(with_metaclass(MetaSingleton, object)):
         # Lock access to the position dicts. This is called from main thread
         # and updates could be happening in the background
         with self._lock_pos:
-            if hasattr(contract, 'm_conId'):
-                position = self.positions[contract.m_conId]
-                if clone:
-                    return copy(position)
+            position = self.positions[contract.m_conId]
+            if clone:
+                return copy(position)
 
-                return position
+            return position
 
     @ibregister
     def updateAccountValue(self, msg):
